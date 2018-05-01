00e891f84e54943294926220a6270f58fcf81885
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-05-01 15:41:46 -0500

    added param to marker func

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import markerMaker from './marker.js';
 import { MapDataEvent } from 'mapbox-gl';
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZHVzbWNkIiwiYSI6ImNqZ28zMWU0YzAwaWcycW54NXJ5ZnZ2bzQifQ._03haGKDdSCU0nRs5dtwEA';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZHVzbWNkIiwiYSI6ImNqZ28zMWU0YzAwaWcycW54NXJ5ZnZ2bzQifQ._03haGKDdSCU0nRs5dtwEA';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
 });
 
-markerMaker('restaurant').addTo(map)
-
+markerMaker('activity', [-74.009, 40.705]).addTo(map);
--- src/marker.js
+++ src/marker.js
@@ -1,26 +1,25 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
-function markerMaker (type){
-    var image;
-    const marker1 = document.createElement('div')
-    marker1.style.width = "32px";
-    marker1.style.height = "39px";
-    switch(type){
-        case 'activity':
-            image =  'url(http://i.imgur.com/WbMOfMl.png)';
-            break;
-        case 'hotel':
-            image =  'url(http://i.imgur.com/D9574Cu.png)';
-            break;
-        case 'restaurant':
-            image =  'url(http://i.imgur.com/cqR6pUI.png)';
-            break;
-        default: 
-            image = 'url(http://i.imgur.com/WbMOfMl.png';  
-    }
-    marker1.style.backgroundImage = image;
-    console.log(marker1)
-    return new mapboxgl.Marker(marker1).setLngLat([-74.009151, 40.705086]);
+function markerMaker(type, coordinates) {
+  var image;
+  const marker1 = document.createElement('div');
+  marker1.style.width = '32px';
+  marker1.style.height = '39px';
+  switch (type) {
+    case 'activity':
+      image = 'url(http://i.imgur.com/WbMOfMl.png)';
+      break;
+    case 'hotel':
+      image = 'url(http://i.imgur.com/D9574Cu.png)';
+      break;
+    case 'restaurant':
+      image = 'url(http://i.imgur.com/cqR6pUI.png)';
+      break;
+    default:
+      image = 'url(http://i.imgur.com/WbMOfMl.png';
+  }
+  marker1.style.backgroundImage = image;
+  return new mapboxgl.Marker(marker1).setLngLat(coordinates);
 }
 
-export default markerMaker
+export default markerMaker;
